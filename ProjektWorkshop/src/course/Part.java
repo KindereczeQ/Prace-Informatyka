a5fad2605b3f97e3534d1d1a00439bd692efbcc8
KindereczeQ <73135314+KindereczeQ@users.noreply.github.com>
2021-08-19 12:32:11 +0200

    Add files via upload

---
+++ ProjektWorkshop/src/course/Part.java
@@ -0,0 +1,69 @@
+package course;
+
+
+
+public class Part {
+	public String name;
+	public int series;
+	public double price;
+	public Part(String name, int series, double price) {
+		super();
+		this.name = name;
+		this.series = series;
+		this.price = price;
+	}
+	public Part(String string, double d) {
+		
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSeries() {
+		return series;
+	}
+
+	public void setSeries(int series) {
+		this.series = series;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+		
+	}
+	
+	}
+	
+		
+		
+		
+		
+		
+		
+		
+	
+		
+	
+	
+	
+	
+
+
+
+
+
+
+	
+
+
+
+
+
